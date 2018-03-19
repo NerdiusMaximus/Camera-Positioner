9ca062afadc73625e476f36425b8d0884e2aa721
hchan02 <hc1177@nyu.edu>
2018-03-19 15:46:25 -0400

    Add files via upload
    getClosestAngle wasn't working. Now it does

--- getClosestAngle.m
+++ getClosestAngle.m
@@ -5,18 +5,12 @@ function [ index ] = getClosestAngle(desiredAngles,data)
 index = [];
 
 for i=1:size(desiredAngles,1)
- 
-    diff = abs(data - desiredAngles(i));
-%     diff = sgolayfilt(diff,3,41);
-    DataInv = 1.01*max(diff) - diff;
-    minpeakheight = 1.01*max(diff) - 9; 
-    minpeakdistance = size(data,1)-1;
     
-    [Minima,MinIdx] = findpeaks(DataInv,'MINPEAKHEIGHT',minpeakheight,...
-        'MINPEAKDISTANCE',minpeakdistance);
-      index = cat(1,index,MinIdx);
-   
-      
+    diff = abs(data - desiredAngles(i));
+    closest = find(diff == min(diff));
+    
+      index = cat(1,index,closest);
 end
+
 
 end
